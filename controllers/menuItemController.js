1e656544a672f5d87ab422456b74fec635e35f1f
FennJessica-FS <JLFenn@student.fullsail.edu>
2026-01-19 20:19:51 -0500

    Complete Module 2 second collection with populate, select, and messages

--- controllers/menuItemController.js
+++ controllers/menuItemController.js
@@ -1,38 +1,63 @@
 const MenuItem = require("../models/MenuItem");
+const Restaurant = require("../models/Restaurant");
+const MSG = require("../messages/messages");
 
 // GET all menu items
 exports.getMenuItems = async (req, res) => {
   try {
-    const items = await MenuItem.find();
-    res.json(items);
+    const items = await MenuItem.find()
+      .select("-__v")
+      .populate("restaurantId", "-__v");
+
+    res.status(200).json(items);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch menu items" });
+    res.status(500).json({ message: MSG.SERVER_ERROR });
   }
 };
 
 // GET menu item by ID
 exports.getMenuItemById = async (req, res) => {
   try {
-    const item = await MenuItem.findById(req.params.id);
+    const item = await MenuItem.findById(req.params.id)
+      .select("-__v")
+      .populate("restaurantId", "-__v");
 
     if (!item) {
-      return res.status(404).json({ error: "Menu item not found" });
+      return res.status(404).json({ message: MSG.NOT_FOUND });
     }
 
-    res.json(item);
+    res.status(200).json(item);
   } catch (error) {
-    res.status(400).json({ error: "Invalid menu item id" });
+    res.status(500).json({ message: MSG.SERVER_ERROR });
   }
 };
 
 // POST create a menu item
 exports.createMenuItem = async (req, res) => {
   try {
-    const newItem = new MenuItem(req.body);
-    const savedItem = await newItem.save();
-    res.status(201).json(savedItem);
+    const { name, price, available, restaurantId } = req.body;
+
+    // make sure restaurant exists
+    const restaurantExists = await Restaurant.findById(restaurantId);
+    if (!restaurantExists) {
+      return res.status(404).json({ message: MSG.NOT_FOUND });
+    }
+
+    const created = await MenuItem.create({
+      name,
+      price,
+      available,
+      restaurantId,
+    });
+
+    // return populated + clean payload
+    const populated = await MenuItem.findById(created._id)
+      .select("-__v")
+      .populate("restaurantId", "-__v");
+
+    res.status(201).json({ message: MSG.CREATED, data: populated });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({ message: MSG.BAD_REQUEST });
   }
 };
 
---
+++ messages/messages.js
@@ -0,0 +1,8 @@
+module.exports = {
+  NOT_FOUND: "Object not found",
+  SERVER_ERROR: "Server error",
+  CREATED: "Created successfully",
+  UPDATED: "Updated successfully",
+  DELETED: "Deleted successfully",
+  BAD_REQUEST: "Bad request",
+};
